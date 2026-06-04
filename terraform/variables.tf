d664151c08e46e9ff0dff8577fd73a19faeca88c
Rajesh <rajeshdangi445566@gmail.com>
2026-06-04 11:47:53 +0530

    testing

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  description = "AWS region where to deploy resources"
+  description = "AWS region where to deploy our resources"
   type        = string
   default     = "ap-south-1"
 }
